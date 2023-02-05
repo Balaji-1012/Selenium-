a41ff371cfe3f2829ee27b4ca700b23d63ebc112
Balaji-1012 <124460278+Balaji-1012@users.noreply.github.com>
2023-02-05 10:49:33 +0530

    Add files via upload

---
+++ day1assignment/Mobile.java
@@ -0,0 +1,25 @@
+package week1.day1assignment;
+
+public class Mobile {
+	public static void main(String[] args) {
+		String mobileBrandName = "OnePlus";
+		System.out.println(mobileBrandName);
+		char mobileLogoo =  'O';
+		System.out.println(mobileLogoo);
+		short noOfMobilePiece = 10;
+		System.out.println(noOfMobilePiece);
+		int modelNumber = 101;
+		System.out.println(modelNumber);
+		long mobileNumber = 45768545335l;
+		System.out.println(mobileNumber);
+		float mobilePrice = 30000f;
+		System.out.println(mobilePrice);
+		boolean isDamaged= false;
+		System.out.println(isDamaged);
+
+		
+		
+		
+	}
+
+}
---
+++ day1assignment/TwoWheeler.java
@@ -0,0 +1,26 @@
+package week1.day1assignment;
+
+public class TwoWheeler {
+public static void main(String[] args) {
+	int noOfWheels = 5;
+	System.out.println(noOfWheels);
+	short noOfMirrors = 6;
+	System.out.println(noOfMirrors);
+	long chassNumber = 4521555l;
+	System.out.println(chassNumber);
+	boolean isPunctured = true;
+	System.out.println(isPunctured);
+	String bikeName = "KTM";
+	System.out.println(bikeName);
+	double runningKm = 4555.4445;
+	System.out.println(runningKm);
+	
+	
+	
+	
+	
+	
+	
+	
+}
+}
